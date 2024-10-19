01895062fee137e14c0e70f33b263ee8ff0e80d1
Arenukvern <anton@xsoulspace.dev>
2024-10-19 04:51:20 +0200

    wip: ui prediction timeline

--- packages/mobile_app/lib/ui_prediction/ui_prediction_timeline.dart
+++ packages/mobile_app/lib/ui_prediction/ui_prediction_timeline.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:mobile_app/common_imports.dart';
+import 'package:skeletonizer/skeletonizer.dart'; // Make sure to add this package to your pubspec.yaml
 
 enum PresentationType { day, month, year }
 
@@ -20,20 +21,32 @@ class UiPredictionTimeline extends StatefulWidget {
 }
 
 class _UiPredictionTimelineState extends State<UiPredictionTimeline> {
-  late ScrollController _scrollController;
-  late List<DateTime> _dates;
-  late int _centerIndex;
-  final _itemExtent = 58.0; // 50 for the circle + 8 for margins
+  late final ScrollController _scrollController;
+  late final List<DateTime> _dates;
+  late int _selectedIndex;
+  final _itemExtent = 58.0;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _initializeDates();
-    _scrollController = ScrollController(
-      initialScrollOffset: _centerIndex * _itemExtent,
-    );
-    WidgetsBinding.instance
-        .addPostFrameCallback((final _) => _setUpScrollListener());
+    unawaited(_load());
+  }
+
+  Future<void> _load() async {
+    await Future.microtask(() {
+      _initializeDates();
+      _scrollController = ScrollController(
+        initialScrollOffset: _selectedIndex * _itemExtent -
+            MediaQuery.sizeOf(context).width / 2 +
+            _itemExtent / 2,
+      );
+    });
+
+    _setUpScrollListener();
+    if (mounted) {
+      setState(() => _isLoading = false);
+    }
   }
 
   void _initializeDates() {
@@ -55,7 +68,7 @@ class _UiPredictionTimelineState extends State<UiPredictionTimeline> {
           (final index) => DateTime(now.year - (5000 - index)),
         );
     }
-    _centerIndex = _dates.indexOf(
+    _selectedIndex = _dates.indexOf(
       _dates.firstWhere(
         (final date) =>
             date.year == widget.initialDate.year &&
@@ -69,45 +82,108 @@ class _UiPredictionTimelineState extends State<UiPredictionTimeline> {
 
   void _setUpScrollListener() {
     _scrollController.addListener(() {
-      final middleIndex = (_scrollController.offset / _itemExtent).round();
-      if (middleIndex != _centerIndex) {
-        setState(() => _centerIndex = middleIndex);
-        widget.onDateChanged(_dates[_centerIndex]);
+      final middleOffset =
+          _scrollController.offset + MediaQuery.sizeOf(context).width / 2;
+      final middleIndex = (middleOffset / _itemExtent).round();
+      if (middleIndex != _selectedIndex &&
+          middleIndex >= 0 &&
+          middleIndex < _dates.length) {
+        setState(() => _selectedIndex = middleIndex);
+        widget.onDateChanged(_dates[_selectedIndex]);
       }
     });
   }
 
+  void _onItemTap(final int index) {
+    setState(() => _selectedIndex = index);
+    widget.onDateChanged(_dates[_selectedIndex]);
+    unawaited(
+      _scrollController.animateTo(
+        index * _itemExtent -
+            MediaQuery.sizeOf(context).width / 2 +
+            _itemExtent / 2,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
   @override
-  Widget build(final BuildContext context) => Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SizedBox(
-            height: 50,
-            child: ListView.builder(
-              controller: _scrollController,
-              scrollDirection: Axis.horizontal,
-              itemExtent: _itemExtent,
-              itemCount: _dates.length,
-              itemBuilder: (final context, final index) => UiPredictionDay(
-                day: _getDisplayText(_dates[index]),
-                isSelected: index == _centerIndex,
+  Widget build(final BuildContext context) => _isLoading
+      ? _buildSkeletonLoader()
+      : Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 50,
+              child: ListView.builder(
+                controller: _scrollController,
+                scrollDirection: Axis.horizontal,
+                itemExtent: _itemExtent,
+                itemCount: _dates.length,
+                itemBuilder: (final context, final index) => UiBaseButton(
+                  onPressed: () => _onItemTap(index),
+                  builder: (final context, final focused, final onlyFocused) =>
+                      UiPredictionDay(
+                    day: _getDisplayText(_dates[index]),
+                    isSelected: index == _selectedIndex,
+                    isCurrentDate: _isCurrentDate(_dates[index]),
+                  ),
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(_getFormattedDate(_dates[_centerIndex])),
-        ],
+            const SizedBox(height: 8),
+            Text(
+              '${_getFormattedDate(_dates[_selectedIndex])}${_isCurrentDate(_dates[_selectedIndex]) ? ' (today)' : ''}',
+            ),
+          ],
+        );
+
+  Widget _buildSkeletonLoader() => Skeletonizer(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 50,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemExtent: _itemExtent,
+                itemCount: 7,
+                itemBuilder: (final context, final index) =>
+                    const UiPredictionDay(
+                  day: 'X',
+                  isSelected: false,
+                  isCurrentDate: false,
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text('Loading...'),
+          ],
+        ),
       );
 
-  String _getDisplayText(final DateTime date) {
+  bool _isCurrentDate(final DateTime date) {
+    final now = DateTime.now();
     switch (widget.presentationType) {
       case PresentationType.day:
-        return DateFormat('E').format(date)[0];
+        return date.year == now.year &&
+            date.month == now.month &&
+            date.day == now.day;
       case PresentationType.month:
-        return DateFormat('MMM').format(date)[0];
+        return date.year == now.year && date.month == now.month;
       case PresentationType.year:
-        return date.year.toString().substring(2);
+        return date.year == now.year;
     }
+  }
+
+  String _getDisplayText(final DateTime date) {
+    final displayText = switch (widget.presentationType) {
+      PresentationType.day => DateFormat('E').format(date)[0],
+      PresentationType.month => DateFormat('MMM').format(date)[0],
+      PresentationType.year => date.year.toString().substring(2),
+    };
+    return displayText;
   }
 
   String _getFormattedDate(final DateTime date) {
@@ -132,24 +208,29 @@ class UiPredictionDay extends StatelessWidget {
   const UiPredictionDay({
     required this.day,
     required this.isSelected,
+    required this.isCurrentDate,
     super.key,
   });
 
   final String day;
   final bool isSelected;
+  final bool isCurrentDate;
 
   @override
-  Widget build(final BuildContext context) => Container(
+  Widget build(final BuildContext context) => AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
         width: 50,
         height: 50,
         margin: const EdgeInsets.symmetric(horizontal: 4),
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           border: Border.all(
-            color: isSelected ? Theme.of(context).primaryColor : Colors.grey,
+            color: isSelected
+                ? Theme.of(context).primaryColor
+                : Theme.of(context).primaryColor.withOpacity(0.1),
             width: isSelected ? 2 : 1,
           ),
-          color: isSelected
+          color: isCurrentDate
               ? Theme.of(context).primaryColor.withOpacity(0.1)
               : null,
         ),
@@ -157,8 +238,12 @@ class UiPredictionDay extends StatelessWidget {
           child: Text(
             day,
             style: TextStyle(
-              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-              color: isSelected ? Theme.of(context).primaryColor : null,
+              fontWeight: isSelected || isCurrentDate
+                  ? FontWeight.bold
+                  : FontWeight.normal,
+              color: isSelected || isCurrentDate
+                  ? Theme.of(context).primaryColor
+                  : null,
             ),
           ),
         ),
