f9a904834cc141325eb158a11436c0d49551d49f
arenukvern <anton@xsoulspace.dev>
2026-08-24 03:11:28 +0300

    wip: assets

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,107 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+class $AssetsGoogleFontsGen {
+  const $AssetsGoogleFontsGen();
+
+  /// File path: assets/google_fonts/NotoSans-Black.ttf
+  String get notoSansBlack => 'assets/google_fonts/NotoSans-Black.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-BlackItalic.ttf
+  String get notoSansBlackItalic =>
+      'assets/google_fonts/NotoSans-BlackItalic.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-Bold.ttf
+  String get notoSansBold => 'assets/google_fonts/NotoSans-Bold.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-BoldItalic.ttf
+  String get notoSansBoldItalic =>
+      'assets/google_fonts/NotoSans-BoldItalic.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-ExtraBold.ttf
+  String get notoSansExtraBold => 'assets/google_fonts/NotoSans-ExtraBold.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-ExtraBoldItalic.ttf
+  String get notoSansExtraBoldItalic =>
+      'assets/google_fonts/NotoSans-ExtraBoldItalic.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-ExtraLight.ttf
+  String get notoSansExtraLight =>
+      'assets/google_fonts/NotoSans-ExtraLight.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-ExtraLightItalic.ttf
+  String get notoSansExtraLightItalic =>
+      'assets/google_fonts/NotoSans-ExtraLightItalic.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-Italic.ttf
+  String get notoSansItalic => 'assets/google_fonts/NotoSans-Italic.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-Light.ttf
+  String get notoSansLight => 'assets/google_fonts/NotoSans-Light.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-LightItalic.ttf
+  String get notoSansLightItalic =>
+      'assets/google_fonts/NotoSans-LightItalic.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-Medium.ttf
+  String get notoSansMedium => 'assets/google_fonts/NotoSans-Medium.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-MediumItalic.ttf
+  String get notoSansMediumItalic =>
+      'assets/google_fonts/NotoSans-MediumItalic.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-Regular.ttf
+  String get notoSansRegular => 'assets/google_fonts/NotoSans-Regular.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-SemiBold.ttf
+  String get notoSansSemiBold => 'assets/google_fonts/NotoSans-SemiBold.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-SemiBoldItalic.ttf
+  String get notoSansSemiBoldItalic =>
+      'assets/google_fonts/NotoSans-SemiBoldItalic.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-Thin.ttf
+  String get notoSansThin => 'assets/google_fonts/NotoSans-Thin.ttf';
+
+  /// File path: assets/google_fonts/NotoSans-ThinItalic.ttf
+  String get notoSansThinItalic =>
+      'assets/google_fonts/NotoSans-ThinItalic.ttf';
+
+  /// File path: assets/google_fonts/OFL.txt
+  String get ofl => 'assets/google_fonts/OFL.txt';
+
+  /// List of all assets
+  List<String> get values => [
+    notoSansBlack,
+    notoSansBlackItalic,
+    notoSansBold,
+    notoSansBoldItalic,
+    notoSansExtraBold,
+    notoSansExtraBoldItalic,
+    notoSansExtraLight,
+    notoSansExtraLightItalic,
+    notoSansItalic,
+    notoSansLight,
+    notoSansLightItalic,
+    notoSansMedium,
+    notoSansMediumItalic,
+    notoSansRegular,
+    notoSansSemiBold,
+    notoSansSemiBoldItalic,
+    notoSansThin,
+    notoSansThinItalic,
+    ofl,
+  ];
+}
+
+abstract final class Assets {
+  static const $AssetsGoogleFontsGen googleFonts = $AssetsGoogleFontsGen();
+}
